2c5925cd653f3dd5b0ec71617d2b60b3fb29426f
Nilesh <nileshkr1706@gmail.com>
2025-08-27 00:27:43 +0530

    Setup Spring Boot project with base dependencies (#1)
    Co-authored-by: Nilesh <nilesh.nilesh@sap.com>

---
+++ api/src/test/java/com/rag/ragbot/RagBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rag.ragbot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RagBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
