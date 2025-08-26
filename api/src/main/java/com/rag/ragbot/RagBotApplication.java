4f51a034d1b86480a5626438a5afa220060ea1b5
Nilesh <nilesh.nilesh@sap.com>
2025-08-27 00:23:56 +0530

    Setup Spring Boot project with base dependencies

---
+++ api/src/main/java/com/rag/ragbot/RagBotApplication.java
@@ -0,0 +1,13 @@
+package com.rag.ragbot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RagBotApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RagBotApplication.class, args);
+	}
+
+}
